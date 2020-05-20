bde6cf512c70815304f2a1932af9948b74ec3750
Hermes Orosco <hermesorosco2010@gmail.com>
2020-05-20 16:47:27 -0300

    agrego puntos 4-5-6b-7b

--- evalPractica1.hs
+++ evalPractica1.hs
@@ -106,3 +106,66 @@ aguaA90Grados vehiculo = vehiculo {temperaturaAgua = 90}
 
 cambio2Llantas :: Auto -> Auto
 cambio2Llantas vehiculo = vehiculo {desgasteLlantas = (\[_,_,c,d]->[0,0,c,d]) (desgasteLlantas vehiculo)}
+
+--Punto 4
+
+auto1 = Auto { patente = "FG884NY", desgasteLlantas= [0.1,0.4,0.2,0] , rpm = 1950, temperaturaAgua = 80, ultimoArreglo = (2,5,2017)}
+auto2 = Auto { patente = "BKQ647", desgasteLlantas= [0.2,0.5,0.6,0.1] , rpm = 2050, temperaturaAgua = 60, ultimoArreglo = (27,9,2018)}
+auto3= Auto { patente = "OFW321", desgasteLlantas= [0.1,0.1,0.1,0] , rpm = 2050, temperaturaAgua = 60, ultimoArreglo = (5,1,2020)}
+
+auto4= Auto { patente = "DVB429", desgasteLlantas= [0.1,0.4,0.2,0] , rpm = 2050, temperaturaAgua = 60, ultimoArreglo = (5,1,2020)}
+auto5= Auto { patente = "VBG546", desgasteLlantas= [0.3,0.5,0.6,0.1] , rpm = 2050, temperaturaAgua = 60, ultimoArreglo = (5,1,2020)}
+auto6= Auto { patente = "MDA381", desgasteLlantas= [0.1,0.1,0.1,0] , rpm = 2050, temperaturaAgua = 60, ultimoArreglo = (5,1,2020)}
+
+auto7= Auto { patente = "DSA429", desgasteLlantas= [0.1,0.4,0.2,0] , rpm = 2050, temperaturaAgua = 60, ultimoArreglo = (5,1,2020)}
+
+auto8= Auto { patente = "AP002TM", desgasteLlantas= [0.1,0.4,0.2,0.1] , rpm = 2050, temperaturaAgua = 60, ultimoArreglo = (5,1,2020)}
+
+--listaAutos1 = [auto1,auto2,auto3]
+
+estanOrdenadosTOC::[Auto]->Bool
+estanOrdenadosTOC [] = True
+estanOrdenadosTOC (a:[]) = odd.(cantidadDesgaste.desgasteLlantas) $ a
+estanOrdenadosTOC (a:b:cz) = (odd.cantidadDesgaste.desgasteLlantas $ a) && (even.cantidadDesgaste.desgasteLlantas $ b) && estanOrdenadosTOC cz
+
+cantidadDesgaste::[Desgaste]->Int
+--cantidadDesgaste :: (Num a) => [a] -> a
+cantidadDesgaste llantasAuto= round.(*10).sum $ llantasAuto
+
+--Punto 5
+listaTec1= [zulu,tango,charly]
+listaTec2= [charly,lima,bravo]
+
+ordenReparacion :: Fecha -> [Mecanico] -> Auto -> Auto
+ordenReparacion unaFecha listaTecnicos unAuto= (actualizarFecha unaFecha) (foldl (flip ($)) unAuto listaTecnicos)
+
+actualizarFecha :: Fecha -> Auto -> Auto
+actualizarFecha unaFecha unAuto= unAuto {ultimoArreglo = unaFecha} 
+--Punto 6
+--parte 1 - integrante a
+--tecnicosQueDejanEnCondiciones listaTecnicos unAuto = map (not esAutoPeligroso (flip ($)) unAuto) listaTecnicos
+
+--parte 2 - integrante b
+auto9 = Auto { patente = "AT001LN", desgasteLlantas= [0.5, 0.15, 0.31, 0.45] , rpm = 2050, temperaturaAgua = 60, ultimoArreglo = (12,9,2014)}
+auto10 = Auto { patente = "DJV215", desgasteLlantas= [0.5, 0.15, 0.31, 0.45] , rpm = 2050, temperaturaAgua = 60, ultimoArreglo = (12,9,2018)}
+auto11 = Auto { patente = "DJV214", desgasteLlantas= [0.5, 0.15, 0.31, 0.45] , rpm = 2050, temperaturaAgua = 60, ultimoArreglo = (12,9,2017)}
+auto12 = Auto { patente = "DFH029", desgasteLlantas= [0.5, 0.15, 0.31, 0.45] , rpm = 2050, temperaturaAgua = 60, ultimoArreglo = (12,9,2015)}
+
+listaAutos2 = [auto9,auto10,auto11,auto12]
+costoReparacionAutosConRevision listaAutos = sum.map costoReparacion.filter (necesitaRevision) $ listaAutos
+
+
+--punto 7
+
+--parte 2
+autosInfinitos :: [Auto]
+autosInfinitos = autosInfinitos' 0
+
+autosInfinitos' :: Float -> [Auto]
+autosInfinitos' n = Auto {
+ patente = "AAA000",
+ desgasteLlantas = [n, 0, 0, 0.3],
+ rpm = 1500 + round n,
+ temperaturaAgua = 90,
+ ultimoArreglo = (20, 1, 2013)
+} : autosInfinitos' (n + 1)
